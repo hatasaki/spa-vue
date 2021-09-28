d1e22a1b9781932931b03118f82437c0f664c099
Keisuke Hatasaki <hatasakick@gmail.com>
2021-09-28 15:56:38 +0900

    change message format

--- api/message/index.js
+++ api/message/index.js
@@ -6,6 +6,6 @@ module.exports = async function (context, req) {
  
     context.res = {
         status: 200, /* Defaults to 200 */
-        body: responseMessage
+        text: responseMessage
     };
 }
