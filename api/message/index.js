6ee2ccc462539bfda3f5122ac699d94f75f838c3
Keisuke Hatasaki <hatasakick@gmail.com>
2021-09-28 15:38:44 +0900

    change function name

--- api/message/index.js
+++ api/message/index.js
@@ -2,10 +2,10 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = "API message - name:"+string(name);
-
+    const responseMessage = "API message="+string(name);
+ 
     context.res = {
-        // status: 200, /* Defaults to 200 */
+        status: 200, /* Defaults to 200 */
         body: responseMessage
     };
 }
